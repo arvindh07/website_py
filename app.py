90e939df8cc0cbbcd1122a33dfd29f6da14f9a56
arvindh07 <59591632+arvindh07@users.noreply.github.com>
2023-08-04 04:34:42 +0000

    added home.html and jobitem.html

--- app.py
+++ app.py
@@ -1,16 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    "id": 0,
+    "name": "Frontend Engineer",
+    "location": "Chennai, India",
+    "salary": "Rs.1,00,000"
+  },
+  {
+    "id": 1,
+    "name": "Fullstack Developer",
+    # "location": "Bangalore, India",
+    "salary": "Rs.2,00,000"
+  },
+  {
+    "id": 2,
+    "name": "Backend Engineer",
+    "location": "Chennai, India",
+    # "salary": "Rs.1,25,000"
+  },
+  {
+    "id": 3,
+    "name": "Data Engineer",
+    "location": "Chennai, India",
+    "salary": "Rs.1,50,000"
+  },
+]
 
 
 @app.route("/")
 def home():
-  return "<p>Hello, World yes!</p>"
+  return render_template("Home.html", jobs=JOBS)
 
-
-@app.route("/about")
-def about():
-  return "<h1>About page</h1>"
+@app.route("/api/jobs")
+def apiJobs():
+  return jsonify(JOBS);
 
 
 if __name__ == "__main__":
